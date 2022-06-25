ae81741651f5d0946c98bee621d0cc511a4a048a
abril443 <108192301+abril443@users.noreply.github.com>
2022-06-25 15:48:13 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,18 @@
+
+$(".button-one ").click(function() {
+    let review= $(".userReview").val();
+    alert(review);
+    $(".reviews").text(review);
+});
+    $(".button-two").click(function() {
+    let reviewTwo= $(".userReviewTwo").val();
+    alert(reviewTwo);
+    $(".reviewsTwo").text(reviewTwo);   
+});
+ $(".button-three").click(function() {
+    let reviewThree= $(".userReviewThree").val();
+    alert(reviewThree);
+    $(".reviewsThree").text(reviewThree);   
+});
+
+
